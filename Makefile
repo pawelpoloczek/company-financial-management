2bab69f32c1371fc449c2b1a934cc9b2e5869b09
Paweł Poloczek <pawel@poloczek.me>
2021-05-31 21:42:02 +0200

    Add github actions

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ build:
 	mv .env.example .env
 	docker-compose build
 	docker-compose up -d --remove-orphans
+	docker exec cfm_php php artisan key:generate
 	docker exec cfm_php php artisan migrate
 start:
 	docker-compose up -d --remove-orphans
---
+++ phpinsights.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use ObjectCalisthenics\Sniffs\Files\ClassTraitAndInterfaceLengthSniff;
+
+return [
+    'preset' => 'laravel',
+    'ide' => 'phpstorm',
+    'exclude' => [],
+    'add' => [],
+    'remove' => [],
+    'config' => [
+        ClassTraitAndInterfaceLengthSniff::class => [
+            'maxLength' => 300,
+        ],
+    ],
+    'requirements' => [
+        'min-quality' => 85,
+        'min-complexity' => 85,
+        'min-architecture' => 90,
+        'min-style' => 95,
+        'disable-security-check' => false,
+    ],
+];
