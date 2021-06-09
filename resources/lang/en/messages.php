842c32d9d5e05a6ed2d48948cb0c2507a70323e6
Paweł Poloczek <pawel@poloczek.me>
2021-06-09 23:11:14 +0200

    Add polish translations for login and registration

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'username' => 'Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'confirm-password' => 'Confirm password',
+    'already-registered' => 'Already registered?',
+    'register' => 'Register',
+    'log-in' => 'Log in',
+    'remember-me' => 'Remember me',
+    'forgot-password' => 'Forgot password?',
+    'password-reset-link' => 'Email Password Reset Link',
+];
---
+++ resources/lang/pl/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'username' => 'Nazwa użytkownika',
+    'email' => 'Adres email',
+    'password' => 'Hasło',
+    'confirm-password' => 'Potwierdź hasło',
+    'already-registered' => 'Aktualnie zarejestrowany?',
+    'register' => 'Zarejestruj się',
+    'log-in' => 'Zaloguj się',
+    'remember-me' => 'Zapamiętaj mnie',
+    'forgot-password' => 'Zapomniałeś hasła?',
+    'password-reset-link' => 'Wyślij link do zresetowania hasła',
+];
